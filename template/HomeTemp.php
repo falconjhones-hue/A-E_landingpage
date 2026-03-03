495bd6f225c6bad1d06c09687e85ac5e6adc8a0f
Aryelson Tevis <tevisaryelson@gmail.com>
2026-03-02 22:16:50 -0300

    corrigindo caminho

--- template/HomeTemp.php
+++ template/HomeTemp.php
@@ -8,20 +8,20 @@ class HomeTemp implements ITemplate
 {
     public function cabecalho()
     {
-        include __DIR__ . "<?= BASE_URL ?>/public/partials/header.php";
-}
-public function rodape()
-{
-include __DIR__ . "<?= BASE_URL ?>/public/partials/footer.php";
-}
-public function layout($caminho, $parametro = null)
-{
-if ($parametro) {
-extract($parametro);
-}
+        include __DIR__ . '/../public/partials/header.php';
+    }
+    public function rodape()
+    {
+        include __DIR__ . '/../public/partials/footer.php';
+    }
+    public function layout($caminho, $parametro = null)
+    {
+        if ($parametro) {
+            extract($parametro);
+        }
 
-$this->cabecalho();
-include __DIR__ . "<?= BASE_URL ?>/" . $caminho . ".php";
-$this->rodape();
-}
+        $this->cabecalho();
+        include __DIR__ . '/../' . $caminho . '.php';
+        $this->rodape();
+    }
 }

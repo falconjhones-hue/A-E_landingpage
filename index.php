14e8fb67588b2ff6e9e43919b020b24cbd731891
Aryelson Tevis <tevisaryelson@gmail.com>
2026-03-02 21:20:57 -0300

    Subindo para o Render

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include "generic/Autoload.php";
 
 use generic\Controller;
 
-define('BASE_URL', 'http://localhost/A-B/');
+define('BASE_URL', 'https://a-b-f574.onrender.com/');
 $rota = $_GET["param"] ?? "landpage/home";
 $controler = new Controller();
 $controler->verificarChamadas($rota);
